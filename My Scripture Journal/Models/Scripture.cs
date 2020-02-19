db08be8835f6183b0d3ddd0da66fe63fc266421d
Rachel Bichsel <rachbichsel@gmail.com>
2020-02-18 23:31:20 -0700

    Update

---
+++ My Scripture Journal/Models/Scripture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace My_Scripture_Journal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        public string Book { get; set; }
+                
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public string Note { get; set; }
+
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+
+
+    }
+}
---
+++ My Scripture Journal/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using My_Scripture_Journal.Data;
+using System;
+using System.Linq;
+
+namespace My_Scripture_Journal.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new My_Scripture_JournalContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<My_Scripture_JournalContext>>()))
+            {
+                // Look for any scriptures.
+                if (context.Scripture.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Scripture.AddRange(
+                    new Scripture
+                    {
+                        Book = "Jacob",
+                        Chapter = 6,
+                        Verse = 12,
+                        Note = "What every parent wants their child to remember.",
+                        DateAdded = DateTime.Parse("2-18-2020")
+                    },
+
+                    new Scripture
+                    {
+                        Book = "1 Nephi",
+                        Chapter = 17,
+                        Verse = 13,
+                        Note = "Favorite Scripture",
+                        DateAdded = DateTime.Parse("1-05-2020")
+                    },
+
+                    new Scripture
+                    {
+                        Book = "1 Nephi",
+                        Chapter = 3,
+                        Verse = 7,
+                        Note = "Go and do! Don't be lazy!",
+                        DateAdded = DateTime.Parse("12-12-2019")
+                    },
+
+                    new Scripture
+                    {
+                        Book = "Matthew",
+                        Chapter = 17,
+                        Verse = 7,
+                        Note = "Don't be afraid. Stand up and have courage.",
+                        DateAdded = DateTime.Parse("4-20-20")
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
